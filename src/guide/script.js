04df7ef3569a037f0e81415aa8273060c76e2cef
TangoCh4rlie <elouanreymond@tutanota.com>
2025-12-04 22:21:10 +0100

    chore: text guide page

---
+++ src/guide/script.js
@@ -0,0 +1,86 @@
+const content = {
+  step1: {
+    title: "Logiciel de Bureautique",
+    grade: "Débutant",
+    text: {
+      title:
+        "Voici la première étape pour reprendre la main sur ta vie numérique",
+      subtitle:
+        "Il éxiste de très nombreuse alternantives opensource / libre pour nos outils de tous les jours.",
+      text: "On peut citer les logiciels de Framasoft ou Infomaniak. Nous vous recommendons les suivant : ",
+      tools: [
+        "Word -> FramaPad / LibreOffice",
+        "Google Form -> FramaForms",
+        "Zoom -> kMeet",
+        "Gmail -> Tutanota",
+        "Navigateur -> Firefox / TOR Browser",
+      ],
+    },
+  },
+  step2: {
+    title: "Personal Computer (PC)",
+    grade: "Connaisseur",
+    text: {
+      title: "Assemblez votre propre ordinateur pour une meilleur réparabilité",
+      subtitle:
+        "Choisissez vos composants en fonction de vos préférences et de vos usages quotidien plus une expérience encore plus personnalisé.",
+      text: "Prenez le temps de bien choisir les composants, vous trouverez de nombreux guides pour vous acompagner dans cette quête.",
+      tools: [
+        "Laptop -> Framework pour un très bon indice de réparabilité",
+        "PC -> Choisissez vos composants, attention à la compatibilité!",
+        "Laptop -> Thinkpad, vous trouverez de nombreux guides sur internet",
+      ],
+    },
+  },
+  step3: {
+    title: "Operating System (OS)",
+    grade: "Aguérri",
+    text: {
+      title: "Ok la ça devient sérieux, fini windows on passe sur Linux !",
+      subtitle:
+        "Vous allez avoir une large panoplie qui s'offre à vous, aventurez vous sur des chemins incertains avec Arch ou sur des plus balisé avec Ubuntu.",
+      text: "En effet, il existe une grande diversité en fonction de l'usage que vous souhaitez au quotidien.",
+      tools: [
+        "Ubuntu / Linux Mint -> Simple, bien documentée, idéale pour découvrir Linux",
+        "Fedora -> Stable, moderne, avec un bon support pour les logiciels bureautiques",
+        "Debian / Arch -> Très personnalisable, idéal pour les développeurs",
+        "Tails -> Pour un maximum d'anonyma parfait avec TOR Browser",
+        "Kali -> Pour les passionnés de cybersécurité",
+      ],
+    },
+  },
+  step4: {
+    title: "Téléphone",
+    grade: "Expert",
+    text: {
+      title:
+        "Itégrez le libre et l'opensource encore plus dans votre quotidien",
+      subtitle:
+        "Certainement une des étapes les plus difficile pour les utilisateur d'IPhone ou de Pixel Phone",
+      text: "Aujourd'hui plusieurs alternantives sont possibles pour customiser votre téléphone",
+      tools: [
+        "ROMs custom -> Changez l'OS de votre téléphone, attention à la compatibilité",
+        "FairPhone -> Pour un indice de réparabilité optimal",
+        "SHIFTphone -> Un produit européen pour les amateurs de RGPD",
+      ],
+    },
+  },
+  step5: {
+    title: "Self Host",
+    grade: "Nerd Ultime",
+    text: {
+      title:
+        "Plus personne ne peux vous tester vous avez complètement repris la main sur votre vie numérique",
+      subtitle:
+        "Hébergez sur votre propre HomeLab (serveur maison) les différents services que vous utilisez au quotidien.",
+      text: "Assemblez une machine (étape 2) puis hébergez vos propres services et faites en profitez votre famille ou vos amis",
+      tools: [
+        "NextCloud -> Un service de stockage de fichier performant et opensource",
+        "Matrix -> Un service de messagerie pour assurer votre vie privée",
+        "Jellyfin -> Fini Netflix, pour des soirées films encore plus savoureuses",
+        "Bitwarden -> Plus besoin de mémoriser vos mots de passes",
+        "Home Assistant -> Pour domotiser votre maison et faire des économies",
+      ],
+    },
+  },
+};
